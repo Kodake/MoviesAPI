6c29d547e2b70eb9b57d5682c1164f23784709bf
Kodake <kodake@hotmail.es>
2022-01-01 23:45:14 -0400

    Adding eager loading
    Adding eager loading to get the Generos from each Movie

---
+++ BackEnd/Controllers/PeliculasController.cs
@@ -0,0 +1,33 @@
+﻿using BackEnd.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeliculasController : ControllerBase
+    {
+        public ApplicationDbContext _context { get; }
+
+        public PeliculasController(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+
+        [HttpGet("{id:int}")]
+        public async Task<ActionResult<Pelicula>> Get(int id)
+        {
+            var pelicula = await _context.Peliculas
+                .Include(p => p.Generos)
+                .FirstOrDefaultAsync(p => p.Id == id);
+
+            if (pelicula == null)
+            {
+                return NotFound();
+            }
+
+            return pelicula;
+        }
+    }
+}
